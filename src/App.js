8f994299dceb3088f2e8c0ce82fb3e4f222c4931
Vincentiusardip <vincentiusardip@gmail.com>
2020-07-25 00:15:34 +0700

    Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Home from './Component/Fungsional/Home';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
