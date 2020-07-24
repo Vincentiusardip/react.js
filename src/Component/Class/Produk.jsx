51a23ded5af5011bb4479ac8345f1f25479c11db
Vincentiusardip <vincentiusardip@gmail.com>
2020-07-25 05:02:10 +0700

    state

--- src/Component/Class/Produk.jsx
+++ src/Component/Class/Produk.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import './CSS/Blog.css';
-const Blog = (props) => {
+import React, {Component} from 'react'
+import './CSS/Produk.css';
+class Produk extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            stock: this.props.stock,
+            sub:"Beli",
+            status: "Tersedia",
+            disabled : false
+        }
 
-    return <div className="blog-wrap">
-        <img src="https://placeimg.com/640/480/tech" alt=""/>
-        <p>{props.tanggal}</p>
-        <h2>{props.judul}</h2>
-        <p>{props.summary }</p>
-    </div>
+    }
+    ButtonBeli = () => {
+
+        this.setState (
+            {
+                stock: this.state.stock -1
+
+        }
+
+        )
+        if(this.state.stock ===1){
+            this.setState({
+                status : "Habis",
+                disabled : true
+            }
+
+            )
+        }
+    }
+    render() {
+
+        return (
+
+            <div className="box-stock">
+                <h2>{this.props.nama}</h2>
+                <img src="https://d2pa5gi5n2e1an.cloudfront.net/global/images/product/laptops/ASUS_ROG_Zephyrus_Duo_15_ph/ASUS_ROG_Zephyrus_Duo_15_ph_L_1.jpg" alt=""/>
+                <p><b>Rp. {this.props.harga}</b></p>
+                <p>{this.state.stock}</p>
+                <button className="btn-click" onClick={this.ButtonBeli} disabled={this.state.disabled}>Beli</button>
+                <p>{this.state.status}</p>
+            </div>
+        )
+    }
 }
-
-export default Blog;
+export default Produk; 
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import Blog from './Blog'
+import Produk from '../Class/Produk';
+//import Blog from './Blog'
 const Home = () => {
-
     return <div>
-        <Blog
-            tanggal="02 Juni 2020"
-            judul="Teknologi Blockchain"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
-        <Blog
-            tanggal="03 Juni 2020"
-            judul="Teknologi Internet Of Things"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
-        <Blog
-            tanggal="04 Juni 2020"
-            judul="Design Pattern Laravel"
-            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
-        />
+    <Produk nama="Asus" stock="12" harga="Rp.12.000.000,-"/>
+    <Produk nama="Acer" stock="30" harga="Rp.8.000.000,-"/>
+    <Produk nama="Axioo" stock="24" harga="Rp.6.000.000,-"/>
+    <Produk nama="Dell" stock="40" harga="Rp.9.000.000,-"/>
+    <Produk nama="Compaq" stock="45" harga="Rp.8.000.000,-"/>
+    <Produk nama="Toshiba" stock="29" harga="Rp.3.600.000,-"/>
     </div>
+
 }
 export default Home;
