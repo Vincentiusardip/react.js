011f9e0605a43a12b2c91feaebe8d316bc4503b8
Vincentiusardip <vincentiusardip@gmail.com>
2020-07-25 21:43:17 +0700

    Router pada React.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
+// import BootstrapComp from './Component/Class/BootstrapComp';
 //import Parent from './Component/Class/Parent';
-//import logo from './logo.svg';
-//import Home from './Component/Fungsional/Home';
-//import Beranda from './Component/Class/Beranda';
-//import './App.css';
-import BootstrapComp from './Component/Class/BootstrapComp';
-function App() {
+// import logo from './logo.svg';
+// import './App.css';
+// import Home from './Component/Fungsional/Home';
+// import Beranda from './Component/Class/Beranda';
+const App = () => {
   return (
-    <div className="App">
-  <BootstrapComp/>
-    </div>
+    <BrowserRouter>
+      <NavbarComp/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
